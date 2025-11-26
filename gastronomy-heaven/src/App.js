ad36f6b0c48c672c37a2eb19ab205f4b630a48fb
SantiagoPard <lspardo@uniboyaca.edu.co>
2025-11-26 04:18:29 -0500

    correccion api url

--- gastronomy-heaven/src/App.js
+++ gastronomy-heaven/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Mail, Phone, MapPin, X } from 'lucide-react';
 // API Base URL
-const API_BASE_URL = 'http://gastronomy-back.vercel.app/api';
+const API_BASE_URL = 'http://gastronomy-back.vercel.app';
 //http://localhost:3001/api
 
 
